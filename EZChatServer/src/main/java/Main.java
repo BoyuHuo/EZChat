dfe0c3ac993b769f8d81c3f0f18a00b45ebb263d
BoyuHuo <boyu.huo.china@gmail.com>
2019-06-04 23:49:21 -0400

    entry point for Server

--- EZChatServer/src/main/java/Main.java
+++ EZChatServer/src/main/java/Main.java
@@ -1,21 +1,10 @@
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
-        primaryStage.setTitle("Hello Server");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-    }
+import service.testTcpServer;
+import java.io.IOException;
 
 
-    public static void main(String[] args) {
-        launch(args);
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new testTcpServer();
     }
 }
