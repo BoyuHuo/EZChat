f26de8a7430918b1a9d95cf5e08eb181dfbd43c3
hengyangan <277265318@qq.com>
2019-06-09 09:40:49 -0400

    suibian

--- EZChatClient/src/main/java/controller/logController.java
+++ EZChatClient/src/main/java/controller/logController.java
@@ -1,4 +1,39 @@
 package controller;
+import javafx.event.*;
+import javafx.scene.control.Button;
+import model.User;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
-public class logController {
+
+public class logController   {
+    @FXML private TextField number;
+    @FXML private TextField password;
+    @FXML private Button log_in;
+
+
+    User User = new User();
+    public void setUser(){
+        User.setUserNumber(number.getText());
+        User.setUserPassword(password.getText());
+    }
+    @FXML public void handleButtonEvent()
+    {
+        if(User.getUserNumber()==User.getUserNumber()){
+            if(User.getUserPassword()==User.getUserPassword()){
+                System.out.println("登录成功");
+                //跳转窗口
+
+            }
+            else System.out.println("登录失败");
+        }
+    }
+
 }
---
+++ EZChatClient/src/main/java/model/User.java
@@ -0,0 +1,31 @@
+package model;
+
+public class User {
+    private String userName;
+    private String userPassword;
+    private String userNumber;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNumber) {
+        this.userNumber = userNumber;
+    }
+}
