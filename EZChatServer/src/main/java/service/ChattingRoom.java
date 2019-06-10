d8e0389569bba49fed2d3c709fa84e7a8c09003e
BoyuHuo <boyu.huo.china@gmail.com>
2019-06-09 22:29:37 -0400

    ChattingRoom  entity and service interface added

---
+++ EZChatServer/src/main/java/service/ChattingRoom.java
@@ -0,0 +1,7 @@
+package service;
+
+public interface ChattingRoom {
+    Boolean CreateChattingRoom(String roomName){
+
+    }
+}
---
+++ EZChatServer/src/main/java/service/imp/ChattingRoomServiceImp.java
@@ -0,0 +1,4 @@
+package service.imp;
+
+public class ChattingRoomServiceImp {
+}
