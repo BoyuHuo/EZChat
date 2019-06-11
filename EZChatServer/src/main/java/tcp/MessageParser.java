47e2c65115b7a7dc8c8df351d0b7a6d829da84b1
cxt714 <cxt714@gmail.com>
2019-06-10 20:44:47 -0400

    Signup message parser process implementation

--- EZChatServer/src/main/java/tcp/MessageParser.java
+++ EZChatServer/src/main/java/tcp/MessageParser.java
@@ -30,7 +30,7 @@ public class MessageParser {
 
 
     public enum Instruction {
-        message, signin, userlist, messagelist;
+        message, signin, userlist, messagelist, signup;
 
         public static Instruction getInstruction(String instruction) {
             return valueOf(instruction.toLowerCase());
@@ -46,20 +46,10 @@ public class MessageParser {
                     System.out.println(name + ":" + tempMsg[2]);
                     break;
                 case signin:
-                    String[] userCredential = tempMsg[2].split("%");
-                    User user = userService.signIn(userCredential[0],userCredential[1]);
-                    if(user!=null){
-                        name = user.getUsername();
-                        TcpServer.user_list.add(name);
-                        TcpServer.thread_list.add(serverThread);
-                        out.println("@signin@yes");
-                        out.println("Hi, "+name + ", Welcome back!");
-                        System.out.println(name + " has signed in!");
-                        serverService.pushMessage(name, " join the chatting room");
-                    }else{
-                        out.println("@signin@no");
-                    }
-
+                    userValidedProcess(tempMsg);
+                    break;
+                case signup:
+                    userSignupProcess(tempMsg);
                     break;
                 case userlist:
                     out.println(serverService.listOnlineUsers());
@@ -76,8 +66,29 @@ public class MessageParser {
     }
 
 
+    public void userValidedProcess(String[] tempMsg) {
+        String[] userCredential = tempMsg[2].split("%");
+        User user = userService.signIn(userCredential[0], userCredential[1]);
+        if (user != null) {
+            name = user.getUsername();
+            TcpServer.user_list.add(name);
+            TcpServer.thread_list.add(serverThread);
+            out.println("@signin@yes@" + user.toString());
+            out.println("Hi, " + name + ", Welcome back!");
+            System.out.println(name + " has signed in!");
+            serverService.pushMessage(name, " join the chatting room");
+        } else {
+            out.println("@signin@no");
+        }
+    }
 
-
+    public void userSignupProcess(String[] tempMsg) {
+        String[] userInfo = tempMsg[2].split(",");
+        User user = new User(userInfo[0], userInfo[1], userInfo[2], userInfo[3], userInfo[4], userInfo[5].replace("*", "@"));
+        Boolean result = userService.signUp(user);
+        String resultStr = result?"yes":"no";
+        out.println("@signup@"+resultStr);
+    }
 
 
     public String getName() {
