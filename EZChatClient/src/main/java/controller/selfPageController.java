45d8fa4bfdd43ac82d143e35932c3db33a658932
MontrealHao <mh2923166@gmail.com>
2019-06-12 19:01:03 -0400

    adding the message in to de message parser and message ecoder for TCP package

--- EZChatClient/src/main/java/controller/selfPageController.java
+++ EZChatClient/src/main/java/controller/selfPageController.java
@@ -1,8 +1,16 @@
 package controller;
 
 import entity.User;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,5 +26,22 @@ public class selfPageController  implements Initializable {
 
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println(user.toString());
+    }
+    @FXML
+    public void createRoomBtn(ActionEvent event) throws IOException {
+        Parent registerScene = FXMLLoader.load(getClass().getResource("/view/createRoom.fxml"));
+        Scene logOnScene = new Scene(registerScene, 300,200);
+        Stage createSceneStage = new Stage();
+        createSceneStage.setScene(logOnScene);
+        createSceneStage.show();
+    }
+    @FXML
+    public void joinRoomBtn(ActionEvent event) throws IOException {
+        Parent registerScene = FXMLLoader.load(getClass().getResource("/view/chat.fxml"));
+        Scene logOnScene = new Scene(registerScene, 800 ,500);
+        Stage createSceneStage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+
+        createSceneStage.setScene(logOnScene);
+        createSceneStage.show();
     }
 }
