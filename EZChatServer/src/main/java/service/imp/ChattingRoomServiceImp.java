6d1bd2b82706fb7a8d3e8788da857306494496af
hengyangan <277265318@qq.com>
2019-06-11 23:44:37 -0400

    update gettoken

--- EZChatServer/src/main/java/service/imp/ChattingRoomServiceImp.java
+++ EZChatServer/src/main/java/service/imp/ChattingRoomServiceImp.java
@@ -3,7 +3,11 @@ package service.imp;
 import entity.ChattingRoom;
 import entity.Message;
 import service.ChattingRoomService;
-
+import java.util.Base64;
+import java.util.Base64.Encoder;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
 import java.util.List;
 
 public class ChattingRoomServiceImp implements ChattingRoomService {
@@ -15,7 +19,19 @@ public class ChattingRoomServiceImp implements ChattingRoomService {
     }
 
     public String generateToken() {
-        //TODO: An hengyang
+        //  7346734837483  834u938493493849384  43434384
+        String token = (System.currentTimeMillis() + new Random().nextInt(999999999)) + "";
+        //数据指纹   128位长   16个字节  md5
+        try {
+            MessageDigest md = MessageDigest.getInstance("md5");
+            byte md5[] = md.digest(token.getBytes());
+            //base64编码--任意二进制编码明文字符
+            Encoder encoder = Base64.getEncoder();
+            String encode = encoder.encodeToString(md5);
+            return encode;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
     public List<ChattingRoom> getHistoryChattingRoomList(String userId) {
