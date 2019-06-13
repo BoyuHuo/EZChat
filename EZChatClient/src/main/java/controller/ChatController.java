c6bce5055db7e2e8d8b0d4523b32aaf3c194f91c
MontrealHao <mh2923166@gmail.com>
2019-06-13 16:42:25 -0400

    join room client end

--- EZChatClient/src/main/java/controller/ChatController.java
+++ EZChatClient/src/main/java/controller/ChatController.java
@@ -1,10 +1,13 @@
 package controller;
 
+import entity.ChattingRoom;
 import javafx.fxml.FXML;
 
 import javax.swing.text.TableView;
 
 public class ChatController {
+
+    public static ChattingRoom room;
 
 
 
--- EZChatClient/src/main/java/controller/SelfPageController.java
+++ EZChatClient/src/main/java/controller/SelfPageController.java
@@ -7,21 +7,29 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 import javafx.stage.Stage;
 import entity.User;
 
 import javafx.fxml.Initializable;
+import service.RoomService;
+import service.imp.RoomServiceImp;
 
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 
 public class SelfPageController implements Initializable {
 
     public static User user;
+
+
+    RoomService roomService = new RoomServiceImp();
 
 
     @FXML Label username;
@@ -44,11 +52,37 @@ public class SelfPageController implements Initializable {
     // input room number to enter room
     @FXML
     private void clickinputRoomNumber(ActionEvent event) throws IOException {
-        Parent newGameScreen = FXMLLoader.load(getClass().getResource("/view/inputRoomNumber.fxml"));
-        Scene newGameScene = new Scene(newGameScreen, 300, 200);
-        Stage newGameScreenStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        newGameScreenStage.setScene(newGameScene);
-        newGameScreenStage.show();
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("Join Room");
+        dialog.setHeaderText("Join Room");
+        dialog.setContentText("Please enter room token:");
+
+        // Traditional way to get the response value.
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()){
+            roomService.joinRoom(result.get());
+            while(RoomServiceImp.getJoinRoomFlag()==0){}
+        }
+        if(RoomServiceImp.getJoinRoomFlag()==1){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Wong Token!");
+            alert.setHeaderText(null);
+            alert.setContentText("Room doesn't exist!");
+            alert.showAndWait();
+        }
+        if(RoomServiceImp.getJoinRoomFlag()==2){
+            ChatController.room = RoomServiceImp.room;
+
+            Parent newGameScreen = FXMLLoader.load(getClass().getResource("/view/chat.fxml"));
+            Scene newGameScene = new Scene(newGameScreen, 800, 500);
+            Stage newGameScreenStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            newGameScreenStage.setScene(newGameScene);
+            newGameScreenStage.show();
+
+        }
+
+
+
     }
 
     // create room
