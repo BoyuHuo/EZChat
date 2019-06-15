1ff03e3cf759d15915817f1c1bde0ea2d0f90bef
rino <rinotrs@gmail.com>
2019-06-15 14:38:11 -0400

    CSS files

--- EZChatClient/src/main/java/Main.java
+++ EZChatClient/src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("view/log.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("EZ Chat");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
--- EZChatClient/src/main/java/controller/SelfPageController.java
+++ EZChatClient/src/main/java/controller/SelfPageController.java
@@ -43,7 +43,7 @@ public class SelfPageController implements Initializable {
     @FXML
     private void clickeditInformation(ActionEvent event) throws IOException {
         Parent newGameScreen = FXMLLoader.load(getClass().getResource("/view/modifyInformation.fxml"));
-        Scene newGameScene = new Scene(newGameScreen, 420, 700);
+        Scene newGameScene = new Scene(newGameScreen, 420, 600);
         Stage newGameScreenStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         newGameScreenStage.setScene(newGameScene);
         newGameScreenStage.show();
